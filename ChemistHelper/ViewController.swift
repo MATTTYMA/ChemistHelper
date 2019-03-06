e3730483d802341c3e7a87442e8e98c9b8dd505c
腾宇Matthew <31982122+ChineseGardenCat@users.noreply.github.com>
2019-03-06 15:32:18 +1100

    Initial Commit

---
+++ ChemistHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChemistHelper
+//
+//  Created by MATTEW MA on 6/3/19.
+//  Copyright © 2019 Mathhew MA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
